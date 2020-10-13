045b0650ce7550f43706254c60953d12cdba7d52
shivatejapeddi <shivatejapeddi@gmail.com>
2020-10-13 15:06:02 +0530

    sdm845-common : Build QTI Thermal hal

--- sdm845.mk
+++ sdm845.mk
@@ -319,6 +319,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
+# Thermal
+PRODUCT_PACKAGES += \
+    android.hardware.thermal@2.0-service.qti
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
