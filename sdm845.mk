3497d123acfd4b1c43a1693806e5be4eac60c418
shivatejapeddi <shivatejapeddi@gmail.com>
2020-10-15 14:44:37 +0530

    sdm845-common : Build Power Aidl

--- sdm845.mk
+++ sdm845.mk
@@ -262,10 +262,11 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service-qti
+    android.hardware.power-service \
+    android.hardware.power-impl
 
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/power/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
 
 # Protobuf
 PRODUCT_PACKAGES += \
